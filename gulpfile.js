c5645fb1b19b443b623090ceebc6d0d0e61e7a43
Anthony Simone <asimone@elevatedthird.com>
2017-02-04 22:11:02 -0700

    Add scss to gulp and refactor index.html to use the stylesheet

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,12 @@
 var gulp = require("gulp");
+var sass = require('gulp-sass');
 var sourcemaps = require("gulp-sourcemaps");
+var autoprefixer = require('gulp-autoprefixer');
 var babel = require("gulp-babel");
-var concat = require("gulp-concat");
 var browserify = require("gulp-browserify");
 var browserSync = require('browser-sync').create();
+var cleanCss = require('gulp-clean-css');
+var rename = require('gulp-rename');
 
 gulp.task('serve', ['scripts'], function() {
 
@@ -12,6 +15,7 @@ gulp.task('serve', ['scripts'], function() {
   });
 
   gulp.watch("./src/**/*.js", ['scripts']).on('change', browserSync.reload);
+  gulp.watch("./src/**/*.scss", ['styles']).on('change', browserSync.reload);
   gulp.watch("./*.html").on('change', browserSync.reload);
 });
 
@@ -23,4 +27,25 @@ gulp.task("scripts", function () {
     //   debug : !gulp.env.production
     // }))
     .pipe(gulp.dest("dist/js"));
+});
+
+gulp.task('styles', function() {
+  gulp.src('src/scss/butterSlideToggle.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    // Catch any SCSS errors and prevent them from crashing gulp
+    .on('error', function (error) {
+      console.error(error);
+      this.emit('end');
+    })
+    .pipe(autoprefixer('last 2 versions', '> 1%', 'ie 9'))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('dist/css/'))
+    .pipe(cleanCss({
+      compatibility: 'ie9'
+    }))
+    .pipe(rename({
+      extname: '.min.css'
+    }))
+    .pipe(gulp.dest('dist/css/'))
 });
