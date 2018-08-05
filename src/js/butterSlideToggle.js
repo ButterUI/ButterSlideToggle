b4e5f3760d1bb2d96f1d8ee816d742d72f252800
Tanner Langley <tlangley141@gmail.com>
2018-08-05 13:36:10 -0600

    Removes dependency on jQuery.

--- src/js/butterSlideToggle.js
+++ src/js/butterSlideToggle.js
@@ -1,149 +1,191 @@
 
-  class ButterSlideToggle {
-    /**
-     * Creates a new instance of ButterSlideToggle.
-     * @class
-     * @fires ButterSlideToggle#init
-     * @param {Object} element - DOM element to initialize as a toggleable element.
-     * @param {Object} options - Overrides to the default plugin settings.
-     */
-    constructor(element, options = {}) {
-      this.element = element;
-      this.$element = $(element);
-      this.options = $.extend({}, ButterSlideToggle.defaults, options);
-      this._collapsed = this.options.beginCollapsed;
+class ButterSlideToggle {
+  /**
+   * Creates a new instance of ButterSlideToggle.
+   * @class
+   * @fires ButterSlideToggle#init
+   * @param {Object} element - DOM element to initialize as a toggleable element.
+   * @param {Object} options - Overrides to the default plugin settings.
+   */
+  constructor(element, options = {}) {
+    this.element = element;
+    this.options = $.extend({}, ButterSlideToggle.defaults, options);
+    this._collapsed = this.options.beginCollapsed;
+    this._events = {};
+    this.events = [
+      'openStart',
+      'opened',
+      'closed'
+    ];
 
-      this._init();
+    this._init();
+  }
+
+  /**
+   * Initializes the ButterSlideToggle plugin.
+   * @function
+   * @private
+   */
+  _init() {
+    let baseEl = this.element,
+      options = this.options;
+
+    // wrap = this.wrapElement(baseEl, 'div');
+    let toggleChildren = baseEl.innerHTML;
+    baseEl.innerHTML = `<div class="butter-slide-inner">${toggleChildren}</div>`;
+    baseEl.classList.add('butter-slide-wrap');
+    baseEl.classList.add(options.transitionClass);
+    baseEl.setAttribute('style', 'overflow: hidden;');
+
+    this.createEvents();
+
+    if (this.isCollapsed()) {
+      baseEl.setAttribute('aria-hidden', true);
+      baseEl.setAttribute('aria-expanded', false);
+      baseEl.setAttribute('style', 'max-height: 0; visibility: hidden');
+    } else {
+      this._collapsed = false;
+      baseEl.setAttribute('aria-expanded', true);
+      baseEl.setAttribute('aria-hidden', false);
+      baseEl.setAttribute('style','visibility: visible');
     }
 
-    /**
-     * Initializes the ButterSlideToggle plugin.
-     * @function
-     * @private
-     */
-    _init() {
-      let $wrap,
-          baseEl = this.element,
-          $baseEl = $(this.element),
-          options = this.options;
+    this.inner = baseEl.children;
+    this._bindAnimationEndListener();
+  }
 
-      $baseEl.wrap(`<div class="butter-slide-wrap ${options.transitionClass}" />`);
-      $wrap = $baseEl.parent();
-      this.$wrap = $wrap;
+  _bindAnimationEndListener() {
+    let wrap = this.element,
+      element = this.inner,
+      thisClass = this;
 
-      $wrap.css({
-        'overflow': 'hidden'
-      });
-
-      if (this.isCollapsed()) {
-        $wrap.attr('aria-hidden', true);
-        $wrap.attr('aria-expanded', false);
-        $wrap.css({
-          maxHeight: 0,
-          visibility: 'hidden'
-        });
-      } else {
-        this._collapsed = false;
-        $wrap.attr('aria-expanded', true);
-        $wrap.attr('aria-hidden', false);
-        $wrap.css({visibility: 'visible'});
-      }
-
-      this._bindAnimationEndListener();
-    }
-
-    _bindAnimationEndListener() {
-      let $wrap = this.$wrap,
-          $element = this.$element,
-          thisClass = this;
-      $wrap.on('transitionEnd webkitTransitionEnd transitionend oTransitionEnd msTransitionEnd', function(e) {
+    [
+      'transitionEnd',
+      'webkitTransitionEnd',
+      'transitionend',
+      'oTransitionEnd',
+      'msTransitionEnd'
+    ].forEach((eventName) => {
+      wrap.addEventListener(eventName, (e) => {
         if (!thisClass._collapsed) {
-          $wrap.css('max-height', 9999);
+          element.setAttribute('style', 'max-height: 9999px;');
           // @TODO: this is getting triggered twice, but it's unclear
-          $element.trigger('butterToggle.opened');
+          this.triggerEvent(wrap, 'opened');
         } else {
-          $element.trigger('butterToggle.closed');
+          this.triggerEvent(wrap, 'closed');
         }
+      }, false);
+    });
+  }
+
+  /**
+   * Toggles the target class on the target element. An event is fired from the original trigger depending on if the resultant state was "on" or "off".
+   * @function
+   */
+  toggle() {
+    let wrap = this.element,
+      element = this.inner,
+      height = this.getAbsoluteHeight(wrap),
+      innerHeight = this.getAbsoluteHeight(element),
+      thisClass = this;
+
+
+    if (thisClass._collapsed) {
+      // If closed, add inner height to content height
+      // TODO: determine if this is the right thing to do, adding both heights together, could this cause lag in accordion? for instance
+      this.triggerEvent(wrap, 'openStart');
+      element.setAttribute('style', `max-height: ${innerHeight + height}; visibility: visible`);
+      thisClass._collapsed = false;
+      element.setAttribute('aria-expanded', true);
+      element.setAttribute('aria-hidden', false);
+    } else {
+      this.triggerEvent(wrap, 'closeStart');
+      // Disable transitions & set max-height to content height
+      element.classList.remove('butter-slide-toggle-transition');
+      element.setAttribute('style', `max-height: ${height}`);
+
+      // Skip an animation frame to ensure that max-height is applied in all browsers.
+      this.skipFrame(() => {
+        thisClass._collapsed = true;
+        element.setAttribute('aria-hidden', true);
+        element.setAttribute('aria-expanded', false);
+        element.classList.add('butter-slide-toggle-transition');
+        element.setAttribute('style', `max-height: 0; visibility: hidden`);
       });
-    }
-
-    /**
-     * Toggles the target class on the target element. An event is fired from the original trigger depending on if the resultant state was "on" or "off".
-     * @function
-     */
-    toggle() {
-      let $wrap = this.$wrap,
-          $element = this.$element,
-          height = $wrap.outerHeight(),
-          innerHeight = $(this.element).outerHeight(),
-          thisClass = this;
-
-      if (thisClass._collapsed) {
-        // If closed, add inner height to content height
-        // TODO: determine if this is the right thing to do, adding both heights together, could this cause lag in accordion? for instance
-        $element.trigger('butterToggle.openStart');
-        $wrap.css('max-height', innerHeight + height);
-        thisClass._collapsed = false;
-        $wrap.attr('aria-expanded', true);
-        $wrap.attr('aria-hidden', false);
-        $wrap.css({
-          maxHeight: innerHeight + height,
-          visibility: 'visible'
-        });
-      } else {
-        $element.trigger('butterToggle.closeStart');
-        // Disable transitions & set max-height to content height
-        $wrap.removeClass('butter-slide-toggle-transition').css('max-height', height);
-
-        // Skip an animation frame to ensure that max-height is applied in all browsers.
-        this.skipFrame(() => {
-          thisClass._collapsed = true;
-          $wrap.attr('aria-hidden', true);
-          $wrap.attr('aria-expanded', false);
-          $wrap.addClass('butter-slide-toggle-transition').css({
-            maxHeight: 0,
-            visibility: 'hidden'
-          });
-        });
-      }
-    }
-
-    /**
-     * Get the animation frame after the next animation frame.
-     * @param callback
-     */
-    skipFrame(callback) {
-      window.requestAnimationFrame(() => {
-        window.requestAnimationFrame(callback);
-      });
-    }
-
-    /**
-     * Public method to access state of the toggle
-     */
-    isCollapsed() {
-      return this._collapsed;
     }
   }
 
-  // Bind to window
-  // window.ButterSlideToggle = ButterSlideToggle;
+  /**
+   * Get the animation frame after the next animation frame.
+   * @param callback
+   */
+  skipFrame(callback) {
+    window.requestAnimationFrame(() => {
+      window.requestAnimationFrame(callback);
+    });
+  }
 
-  ButterSlideToggle.defaults = {
-    /**
-     * Tells the plugin the length of the animation.
-     * @option
-     * @type {string}
-     * @default butter-slide-toggle-transition
-     */
-    transitionClass: 'butter-slide-toggle-transition',
+  /**
+   * Public method to access state of the toggle
+   */
+  isCollapsed() {
+    return this._collapsed;
+  }
 
-    /**
-     * Determines if the toggle content should begin expanded or collapsed.
-     * @option
-     * @type {bool}
-     * @default false
-     */
-    beginCollapsed: false
-  };
+  createEvents() {
+    this.events.forEach((event) => {
+      this._events[event] = new Event(`butterToggle.${event}`);
+    });
+  }
 
+  triggerEvent(el, eventName) {
+    let events = this._events;
+    if (events[eventName]) {
+      el.dispatchEvent(events[eventName]);
+    }
+  }
+
+  /**
+   * Get the absolute height of an element. Similar to jQuery .outerHeight().
+   * Source: https://stackoverflow.com/a/23749355/4091860
+   * @param el
+   * @returns {number}
+   */
+  getAbsoluteHeight(el) {
+    // Get the DOM Node if you pass in a string
+    el = (typeof el === 'string') ? document.querySelector(el) : el;
+
+    if (el.nodeType === Node.ELEMENT_NODE) {
+      let styles = window.getComputedStyle(el);
+      let margin = parseFloat(styles['marginTop']) +
+        parseFloat(styles['marginBottom']);
+
+      return Math.ceil(el.offsetHeight + margin);
+    }
+    return null;
+  }
+}
+
+
+// Bind to window
+// window.ButterSlideToggle = ButterSlideToggle;
+
+ButterSlideToggle.defaults = {
+  /**
+   * Tells the plugin the length of the animation.
+   * @option
+   * @type {string}
+   * @default butter-slide-toggle-transition
+   */
+  transitionClass: 'butter-slide-toggle-transition',
+
+  /**
+   * Determines if the toggle content should begin expanded or collapsed.
+   * @option
+   * @type {bool}
+   * @default false
+   */
+  beginCollapsed: false
+};
+
+export default ButterSlideToggle;
